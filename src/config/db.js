316c5211e5d05b32eadaf3d52f85cadff73e81e2
adechotechnologies <152974710+adechotechnologies@users.noreply.github.com>
2025-06-21 23:41:01 -0700

    debuggin

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
+    // , {
+    //   // useNewUrlParser: true,
+    //   // useUnifiedTopology: true,
+    // }
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error.message);
